ebf69fe4542db463526f4390f70f19b37a0e4f05
Thelma-Dev <osifoaisosa@gmail.com>
2022-11-08 20:36:25 -0600

    Add color switch

---
+++ assets/script/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const btn = document.querySelector('.switch');
+const boxes = document.querySelectorAll('.box');
+
+
+
+let colors = ['#FF8C00', '#FFD700', '#ff00ff', '#008080', '#BC8F8F', '#708090', '#D8BFD8', '#CD5C5C', '#BDB76B'];
+
+btn.addEventListener("click", function () {
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+  }
+});
